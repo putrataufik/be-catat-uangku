dc95aeebdc002df56abb4bee03a339fd25682c69
putrataufik <daengkadaeng@gmail.com>
2025-06-08 01:39:14 +0800

    update middlewares

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,25 +1,22 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
 const JWT_SECRET = process.env.JWT_SECRET || 'supersecret';
+const User = require("../models/userModel");
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
+  const token = req.headers.authorization?.split(' ')[1]; // Bearer token
 
   if (!token) return res.status(401).json({ message: 'Akses ditolak. Token tidak ditemukan.' });
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
-
-    // GANTI DARI decoded.id KE decoded.userId
     const user = await User.findById(decoded.userId);
-    if (!user) {
-      return res.status(404).json({ message: 'User tidak ditemukan' });
-    }
 
-    req.user = user;
+    if (!user) {
+      return res.status(404).json({ message: "User tidak ditemukan" });
+    }
+    req.user = decoded; // menyimpan payload di request
     next();
   } catch (err) {
-    console.error('Auth error:', err);
     res.status(403).json({ message: 'Token tidak valid' });
   }
 };
